d49b8084959c9914487f84478e5a2bf3dd700310
clsld <807686672@qq.com>
2020-12-26 13:03:48 +0800

    加入了发布页面，买过的页面，收藏页面的样式和获取数据库的逻辑js

--- miniprogram/pages/gouwuche/gouwuche.js
+++ miniprogram/pages/gouwuche/gouwuche.js
@@ -1,20 +1,38 @@
 // miniprogram/pages/gouwuche/gouwuche.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    listType: 1, // 1为1个商品一行，2为2个商品一行    
+    openid: '', // 搜索关键词
+    orderBy: '', // 排序规则
+    goods: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options.openid)
+    this.setData({
+      openid: options.openid,
+    }) 
+    var that = this
+    db.collection("user").where({
+      _openid:this.data.openid
+    }).get({
+      success: function (res) {
+        console.log(res.data[0].goumai)
+        that.setData({
+          goods: res.data[0].goumai
+        })
+      }
+    })
 
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- miniprogram/pages/shouchan/shouchan.js
+++ miniprogram/pages/shouchan/shouchan.js
@@ -1,17 +1,36 @@
 // miniprogram/pages/shouchan/shouchan.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    listType: 1, // 1为1个商品一行，2为2个商品一行    
+    openid: '', // 搜索关键词
+    orderBy: '', // 排序规则
+    goods: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options.openid)
+    this.setData({
+      openid: options.openid,
+    }) 
+    var that = this
+    db.collection("user").where({
+      _openid:this.data.openid
+    }).get({
+      success: function (res) {
+        console.log(res.data[0].shouchan)
+        that.setData({
+          goods: res.data[0].shouchan
+        })
+      }
+    })
 
   },
 
