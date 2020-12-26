b71f1c8b6af1d8e874d5b1e36441c282a7a15f18
ye4431978 <13544303755>
2020-12-26 13:15:35 +0800

    发布

--- miniprogram/pages/fabu/fabu.js
+++ miniprogram/pages/fabu/fabu.js
@@ -5,12 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    array: ['启蒙图书', '科普图书', '历史图书', '卡通图书'],
   },
-
+  chooseImage:function(){
+    var that = this
+    wx.chooseImage({
+      count:1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success (res) {
+        // 返回选定照片的路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        that.setData({img: tempFilePaths})
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
   onLoad: function (options) {
 
   },
